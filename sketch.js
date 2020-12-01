ddf5d198430ef7d0eaccdb56ac27e87a27ed907a
WilliamWhitehat <74245719+WilliamWhitehat@users.noreply.github.com>
2020-12-01 11:55:49 +0000

    Upload 1

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var car
+var wall
+
+var speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+ car = createSprite(400, 200, 50, 50);
+
+speed=random(55,90);
+weight=random(400,1500)
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
